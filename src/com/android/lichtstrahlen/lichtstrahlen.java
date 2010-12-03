fcd873ae8dbf20c71f27e2fd1b7df9979e07b5ac
escoand <escoand@users.noreply.github.com>
2010-12-03 02:58:16 +0100

    verse list completed info page added

--- src/com/android/lichtstrahlen/lichtstrahlen.java
+++ src/com/android/lichtstrahlen/lichtstrahlen.java
@@ -1,11 +1,11 @@
 package com.android.lichtstrahlen;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Vector;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -17,7 +17,6 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.net.Uri;
@@ -28,12 +27,17 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 public class lichtstrahlen extends Activity {
+	static final int DIALOG_ABOUT_ID = 0;
+	static final int DIALOG_DATE_ID = 1;
+	
+	
 	private Date date = new Date();
 	private CharSequence[] verses = null;
 
@@ -47,11 +51,8 @@ public class lichtstrahlen extends Activity {
 	private TextView txtHeader;
 	private TextView txtText;
 	private TextView txtAuthor;
-	
-	private CharSequence[] lstVerses;
 
     private ProgressDialog progress;
-    private AlertDialog alert;
     private AlertDialog selection;
 
 	@Override
@@ -70,15 +71,6 @@ public class lichtstrahlen extends Activity {
         txtHeader = (TextView) findViewById(R.id.headline);
         txtText = (TextView) findViewById(R.id.text);
         txtAuthor = (TextView) findViewById(R.id.author);
-        
-        // init dialogs
-        alert = new AlertDialog.Builder(this).create();
-		alert.setCancelable(false);
-		alert.setButton("OK", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-			}
-		});
         
         // read today
 		if(txtText.getText().equals(""))
@@ -107,8 +99,10 @@ public class lichtstrahlen extends Activity {
  	public boolean onOptionsItemSelected(MenuItem item) {
 		switch(item.getItemId()) {
 		case R.id.menuBible:
-			if(verses.length == 1)
+			if(verses.length == 1) {
 				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.bibleserver.com/text/LUT/" + verses[0])));
+				return true;
+			}
 			else if(verses.length > 1) {
 				AlertDialog.Builder adb = new AlertDialog.Builder(this);
 				adb.setCancelable(true);
@@ -123,14 +117,13 @@ public class lichtstrahlen extends Activity {
 				selection.show();
 				return true;
 			}
-			else
-				return false;
+			return false;
 		case R.id.menuToday:
 			date = new Date();
 			new ReadingTask().execute();
 			return true;
 		case R.id.menuDate:
-			showDialog(0);
+			showDialog(DIALOG_DATE_ID);
 			return true;
 		case R.id.menuNotes:
 			return true;
@@ -138,13 +131,7 @@ public class lichtstrahlen extends Activity {
 			new VerseListTask().execute();
 			return true;
 		case R.id.menuInfo:
-			String version = "";
-			try {
-				version = " " + getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
-			} catch (NameNotFoundException e) { }
-			alert.setTitle(getString(R.string.app_name) + version);
-			alert.setMessage(getString(R.string.aboutMessage));
-			alert.show();
+			showDialog(DIALOG_ABOUT_ID);
 			return true;
 		default:
 			return false;
@@ -157,8 +144,13 @@ public class lichtstrahlen extends Activity {
 	protected Dialog onCreateDialog(int id) {
 		Date date = new Date();
 		switch(id) {
-		case 0:
-			return new DatePickerDialog(this, datepickerlistener, date.getYear() + 1900, date.getMonth(), date.getDate());
+			case DIALOG_DATE_ID:
+				return new DatePickerDialog(this, datepickerlistener, date.getYear() + 1900, date.getMonth(), date.getDate());
+			case DIALOG_ABOUT_ID:
+				Dialog dialog = new Dialog(this);
+				dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+				dialog.setContentView(R.layout.about);
+				return dialog;
 		}
 		return null;
 	}
@@ -320,45 +312,49 @@ public class lichtstrahlen extends Activity {
 		
 		@Override
 		protected ArrayList<HashMap<String, String>> doInBackground(Object... params) {
+			Date curdate = (Date) date.clone();
 			ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String,String>>();
 			XmlPullParser xml = null;
-			int id;
 			String dateread = ""; 
-			
-			// open xml file
-			DisplayMetrics metrics = new DisplayMetrics();
-			getWindowManager().getDefaultDisplay().getMetrics(metrics);
-			id = new Resources(getAssets(), metrics, null).getIdentifier("data_" + new SimpleDateFormat("yyyyMM").format(date), "xml", "com.android.lichtstrahlen");
-			metrics = null;
-			if(id == 0)
-				id = R.xml.data;
-			xml = getResources().getXml(id);
-			
-			try {
-				while(xml.getEventType() != XmlPullParser.END_DOCUMENT) {
-					switch(xml.getEventType()) {
-						case XmlPullParser.START_TAG:
-							if(xml.getName().equals("entry"))
-								dateread = xml.getAttributeValue(null, "date");
-							else if(xml.getName().equals("verse")) {
-								xml.next();
-								HashMap<String, String> item = new HashMap<String, String>();
-								item.put("date", dateread);
-								item.put("verse", xml.getText());
-								list.add(item);
-							}
-							break;
-						case XmlPullParser.END_TAG:
-							if(xml.getName().equals("entry"))
-								dateread = "";;
-							break;
-					}
-					xml.next();
-				}
+
+			// open xml files
+			for(int i = 1; i <= 12; i++) {
+				curdate.setMonth(i - 1);
+				DisplayMetrics metrics = new DisplayMetrics();
+				getWindowManager().getDefaultDisplay().getMetrics(metrics);
+				int id = new Resources(getAssets(), metrics, null).getIdentifier("data_" + new SimpleDateFormat("yyyyMM").format(curdate), "xml", "com.android.lichtstrahlen");
+				metrics = null;
+				if(id == 0)
+					id = R.xml.data;
+				xml = getResources().getXml(id);
 				
-			} catch (Exception e) {
-				e.printStackTrace();
+				try {
+					while(xml.getEventType() != XmlPullParser.END_DOCUMENT) {
+						switch(xml.getEventType()) {
+							case XmlPullParser.START_TAG:
+								if(xml.getName().equals("entry"))
+									dateread = xml.getAttributeValue(null, "date");
+								else if(xml.getName().equals("verse")) {
+									xml.next();
+									HashMap<String, String> item = new HashMap<String, String>();
+									item.put("date", DateFormat.getDateInstance().format(new SimpleDateFormat("yyyyMMdd").parse(dateread)));
+									item.put("verse", xml.getText());
+									list.add(item);
+								}
+								break;
+							case XmlPullParser.END_TAG:
+								if(xml.getName().equals("entry"))
+									dateread = "";;
+								break;
+						}
+						xml.next();
+					}
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
+			
 			return list;
 		}
 
@@ -368,11 +364,17 @@ public class lichtstrahlen extends Activity {
 			adb.setCancelable(true);
 			adb.setTitle(getString(R.string.select));
 			adb.setAdapter(
-				new SimpleAdapter(lichtstrahlen.this, result,R.layout.verselist, new String[] {"verse", "date"}, new int[] {R.id.listVerse, R.id.listDate}),
+				new SimpleAdapter(lichtstrahlen.this, result,R.layout.list, new String[] {"verse", "date"}, new int[] {R.id.listVerse, R.id.listDate}),
 				new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int item) {
-						startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.bibleserver.com/text/LUT/" + verses[item])));
+						try {
+							HashMap<String, String> element = (HashMap<String, String>) selection.getListView().getItemAtPosition(item);
+							date = DateFormat.getDateInstance().parse(element.get("date"));
+							new ReadingTask().execute();
+						} catch (ParseException e) {
+							e.printStackTrace();
+						}
 					}
 			});
 			selection = adb.create();
