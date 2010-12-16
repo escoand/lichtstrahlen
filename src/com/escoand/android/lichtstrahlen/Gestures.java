330888e7c855f58025b33f32c2e47d433dbecc3f
escoand <escoand@users.noreply.github.com>
2010-12-16 14:28:03 +0100

    more straight fling gesture strings optimized question for note deletion other minor fixes and tweaks

--- src/com/escoand/android/lichtstrahlen/Gestures.java
+++ src/com/escoand/android/lichtstrahlen/Gestures.java
@@ -8,7 +8,8 @@ import android.view.MotionEvent;
  * @author escoand
  */
 public class Gestures extends SimpleOnGestureListener {
-    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int X_MIN_DISTANCE = 120;
+    private static final int Y_MAX_DISTANCE = 20;
 
 	private lichtstrahlen context = null;
 
@@ -19,11 +20,11 @@ public class Gestures extends SimpleOnGestureListener {
 
 	@Override
 	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,	float velocityY) {
-		if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE) {
+		if(e1.getX() - e2.getX() > X_MIN_DISTANCE && Math.abs(e1.getY() - e2.getY()) < Y_MAX_DISTANCE) {
             context.nextDay();
             return true;
 		}
-		else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE) {
+		else if (e2.getX() - e1.getX() > X_MIN_DISTANCE && Math.abs(e1.getY() - e2.getY()) < Y_MAX_DISTANCE) {
             context.prevDay();
             return true;
 		}
