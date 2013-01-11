d78926cc0b95db91ae9db914806d169a3c9337b1
escoand <escoand@users.noreply.github.com>
2013-01-11 12:27:46 +0100

    fixed bug with menu
    Conflicts:
    	src/com/escoand/android/lichtstrahlen/MainActivity.java

--- src/com/escoand/android/lichtstrahlen/MainActivity.java
+++ src/com/escoand/android/lichtstrahlen/MainActivity.java
@@ -134,15 +134,16 @@ public class MainActivity extends Activity {
 	public boolean onPrepareOptionsMenu(Menu menu) {
 		MenuItem item = null;
 
-		/* no data */
-		if (menu != null) {
-			item = menu.findItem(R.id.menuBible);
-			if (item != null)
-				item.setEnabled(data_day != null && data_day.getCount() != 0);
-			item = menu.findItem(R.id.menuShare);
-			if (item != null)
-				item.setEnabled(data_day != null && data_day.getCount() != 0);
-		}
+		if (menu == null)
+			return false;
+
+		/* (un)hide items */
+		item = menu.findItem(R.id.menuBible);
+		if (item != null)
+			item.setEnabled(data_day != null && data_day.getCount() != 0);
+		item = menu.findItem(R.id.menuShare);
+		if (item != null)
+			item.setEnabled(data_day != null && data_day.getCount() != 0);
 
 		return super.onPrepareOptionsMenu(menu);
 	}
