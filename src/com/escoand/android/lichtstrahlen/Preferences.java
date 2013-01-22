4dee7bbebf7a2132755c94e2eaccabf639e123d0
escoand <mail-an-papierkorb@web.de>
2013-01-23 00:37:59 +0100

    fixed cancel button at reminder

--- src/com/escoand/android/lichtstrahlen/Preferences.java
+++ src/com/escoand/android/lichtstrahlen/Preferences.java
@@ -19,9 +19,9 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.DialogInterface;
-import android.content.DialogInterface.OnCancelListener;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
@@ -34,7 +34,8 @@ import android.widget.TimePicker;
 import com.escoand.android.lichtstrahlen_2013.R;
 
 public class Preferences extends PreferenceActivity {
-	public static final int DIALOG_REMIND_ID = 3;
+	private static SharedPreferences prefs;
+	private static final int DIALOG_REMIND_ID = 3;
 
 	OnPreferenceChangeListener changed = new OnPreferenceChangeListener() {
 		@Override
@@ -52,6 +53,8 @@ public class Preferences extends PreferenceActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+
+		prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
 
 		/* restart after changed */
 		findPreference("inverse").setOnPreferenceChangeListener(changed);
@@ -82,10 +85,8 @@ public class Preferences extends PreferenceActivity {
 		case DIALOG_REMIND_ID:
 
 			/* get settings */
-			int hour = PreferenceManager.getDefaultSharedPreferences(
-					getBaseContext()).getInt("remind_hour", 9);
-			int minute = PreferenceManager.getDefaultSharedPreferences(
-					getBaseContext()).getInt("remind_minute", 0);
+			int hour = prefs.getInt("remind_hour", 9);
+			int minute = prefs.getInt("remind_minute", 0);
 
 			/* dialog */
 			TimePickerDialog dialog = new TimePickerDialog(this,
@@ -93,51 +94,48 @@ public class Preferences extends PreferenceActivity {
 						@Override
 						public void onTimeSet(TimePicker view, int hour,
 								int minute) {
-							PreferenceManager
-									.getDefaultSharedPreferences(
-											getBaseContext()).edit()
-									.putBoolean("remind", true)
-									.putInt("remind_hour", hour)
+							prefs.edit().putInt("remind_hour", hour)
 									.putInt("remind_minute", minute).commit();
-							((CheckBoxPreference) findPreference("remind"))
-									.setChecked(true);
 							initReminder();
 						}
 					}, hour, minute, true);
+
+			/* ok button */
+			dialog.setButton(DialogInterface.BUTTON_POSITIVE,
+					getString(android.R.string.ok), new OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							initReminder(true);
+						}
+					});
 
 			/* cancel button */
 			dialog.setButton(DialogInterface.BUTTON_NEGATIVE,
 					getString(android.R.string.cancel), new OnClickListener() {
 						@Override
 						public void onClick(DialogInterface dialog, int which) {
-							PreferenceManager
-									.getDefaultSharedPreferences(
-											getBaseContext()).edit()
-									.putBoolean("remind", false).commit();
-							((CheckBoxPreference) findPreference("remind"))
-									.setChecked(false);
-							startService(new Intent(getBaseContext(),
-									Reminder.class));
-							initReminder();
+							initReminder(false);
 						}
 					});
 
 			/* back key */
-			dialog.setOnCancelListener(new OnCancelListener() {
+			dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
 				@Override
 				public void onCancel(DialogInterface dialog) {
-					PreferenceManager
-							.getDefaultSharedPreferences(getBaseContext())
-							.edit().putBoolean("remind", false).commit();
-					((CheckBoxPreference) findPreference("remind"))
-							.setChecked(false);
-					initReminder();
+					initReminder(false);
 				}
 			});
 
 			return dialog;
 		}
+
 		return super.onCreateDialog(id);
+	}
+
+	private void initReminder(boolean state) {
+		prefs.edit().putBoolean("remind", state).commit();
+		((CheckBoxPreference) findPreference("remind")).setChecked(state);
+		initReminder();
 	}
 
 	private void initReminder() {
